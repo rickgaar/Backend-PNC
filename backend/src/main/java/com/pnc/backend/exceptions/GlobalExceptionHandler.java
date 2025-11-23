d14f48104b634f94cee0f67dfc46dd5638ab5001
Joma203 <00120422@uca.edu.sv>
2025-11-23 15:15:18 -0600

    fix: bug in global exception handler

--- backend/src/main/java/com/pnc/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/pnc/backend/exceptions/GlobalExceptionHandler.java
@@ -4,15 +4,30 @@ package com.pnc.backend.exceptions;
 import com.pnc.backend.dto.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.time.LocalDate;
 import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return buildErrorResponse(e, HttpStatus.BAD_REQUEST, errors);
+    }
 
     public ResponseEntity<ApiErrorResponse> buildErrorResponse(Exception e, HttpStatus status, Object data){
         String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().getPath();
