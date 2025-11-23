b05e6bbf529fb12d90e2b60da005e97ba0ad8ad8
Ricardo Garcia <00148922@uca.edu.sv>
2025-11-23 17:36:54 -0600

    fix: url backend

--- backend/src/main/java/com/pnc/backend/controller/MateriaController.java
+++ backend/src/main/java/com/pnc/backend/controller/MateriaController.java
@@ -114,7 +114,7 @@ public class MateriaController {
         MateriaResponse response = materiaService.findWithDetails(id);
         response.getUsuarios().forEach(u -> {
             if (u.getAvatar() != null && !u.getAvatar().isEmpty() && !u.getAvatar().startsWith("http")) {
-                u.setAvatar("http://localhost:8080/api/user/avatar/" + u.getAvatar());
+                u.setAvatar("https://backend-pnc-production-d8ff.up.railway.app/api/user/avatar/" + u.getAvatar());
             }
         });
         return buildResponse("Materia encontrada exitosamente", HttpStatus.OK, response);
